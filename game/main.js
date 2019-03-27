f3cefcf9077208652061a2882e3ca1c013de8dc4
upEverest <48710974+upEverest@users.noreply.github.com>
2019-03-27 19:22:31 +0300

    Add files via upload

--- game/main.js
+++ game/main.js
@@ -1,37 +1,75 @@
-var count = 0;
+var count = 1;
+var count1 = 1;
 var but = document.querySelector(".button");
+var but1 = document.querySelector(".button1");
+let computer_number;
+let computer_number1;
 
-function generate(){
-    return  Math.round(Math.random() * 10);
+
+but.addEventListener("click", generate);
+but.addEventListener("click", game);
+
+function generate() {
+    computer_number = Math.round(Math.random() * 10);
+    console.log(computer_number)
 }
-but.onclick = game;
 
 function game() {
-    let computer_number = generate();
-    console.log(computer_number);
-    var answer = prompt("Попробуй угадать число");
+    var answer = prompt("Попробуй угадать число от 0 до 10");
     if (answer > computer_number) {
         if (count >= 10) {
             alert("Ты проиграл");
-            alert("Число попыток:" + count);
-            return;
+            alert("Число неправильных ответов " + count);
+            return count = 1;
         }
-        alert("Ты взял многовато");
+        alert("Это число меньше " + answer);
         count++;
         game()
-
     } else if (answer < computer_number) {
         if (count >= 10) {
             alert("Ты проиграл");
-            alert("Число попыток:" + count);
-            return;
+            alert("Число неправильных ответов " + count);
+            return count = 1;
         }
-        alert("Ты взял маловато");
+        alert("Это число больше " + answer);
         count++;
+        game()
+    } else {
+        alert("Ты победил с/со "  +  count + "-ой" + " попытки");
+        return count = 1;
+    }
+}
+
+but1.addEventListener("click", generate1);
+but1.addEventListener("click", game1);
+
+function generate1() {
+    computer_number1 = Math.round(Math.random() * 10);
+    console.log(computer_number1)
+}
+
+function game1() {
+    var answer = prompt("Попробуй угадать число от 0 до 10 с трех попыток");
+    if (answer > computer_number1) {
+        if (count1 >= 3) {
+            alert("Ты проиграл");
+            alert("Число неправильных ответов больше " + count1);
+            return count1 = 1;
+        }
+        alert("Это число меньше " + answer);
+        count1++;
+        game1()
+    } else if (answer < computer_number1) {
+        if (count1 >= 3) {
+            alert("Ты проиграл");
+            alert("Число неправильных ответов больше  " + count1);
+            return count1 = 1;
+        }
+        alert("Это число больше " + answer);
+        count1++;
         game1()
     } else {
-        alert("ты победил");
-        alert("Число попыток:" + count);
-        return count = 0;
-        }
+        alert("Ты победил с/со "  +  count1 + "-ой" + " попытки");
+        return count1 = 1;
+    }
 }
